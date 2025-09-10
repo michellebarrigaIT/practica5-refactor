6d7d0b51f4ae3618f3d11f63f99e0daeb5e1c980
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-09-10 11:16:37 -0400

    refactor: refactor functions

--- src/index.js
+++ src/index.js
@@ -6,30 +6,30 @@ console.log("Subtotal:", calculateSubtotal());
 let productName = "Espresso Machine";
 const prices = [120, 30, 15]; // base price, shipping, handling
 
-function calculateSubtotal() {
+const calculateSubtotal = () => {
     return prices[0] + prices[1] + prices[2];
-}
+};
 
 console.log("Subtotal after assignment:", calculateSubtotal()); // C
 
 const discountRate = getDefaultDiscount(); // D
 console.log("Discount rate:", discountRate); // E
 
-function getDefaultDiscount() {
+const getDefaultDiscount = () => {
     return discountValue; // discountValue isn’t declared yet!
 }
 const discountValue = 0.1;
 
 console.log("Discount rate after assignment:", getDefaultDiscount()); // F
 
-const applyDiscount = function(amount) {
+const applyDiscount = (amount) =>{
     return amount - amount * discountValue;
 };
 console.log("Total after discount:", applyDiscount(calculateSubtotal())); // G
 
 console.log("Taxed total (incl. 8%):", addTax(applyDiscount(calculateSubtotal()))); // H
 
-const addTax = (function() {
+const addTax = (() => {
     const rate = 0.08;
     return function(amount) {
         return amount + amount * rate;
